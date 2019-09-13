6389a18cf8e2335a412112fec58544f08d35ecce
Sebastian Gryczan <Sebastian.Gryczan@netapp.com>
2019-09-12 20:45:16 -0600

    Add environment prefix

--- nks/cmd/root.go
+++ nks/cmd/root.go
@@ -64,6 +64,7 @@ func initConfig() {
 		viper.SetConfigName(".nks")
 	}
 
+	viper.SetEnvPrefix("nks") // NKS_<whatever>
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
