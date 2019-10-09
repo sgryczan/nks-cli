3fc7210bebc3eaa6558b770af80a3962c01c4c4d
Sebastian Gryczan <Sebastian.Gryczan@netapp.com>
2019-10-09 00:27:41 -0600

    Improve versioning

--- nks/cmd/version.go
+++ nks/cmd/version.go
@@ -6,13 +6,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version string
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "display CLI version",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("NKS CLI Version - v0.0.1 (alpha)")
+		printVersion(version)
 	},
+}
+
+func printVersion(v string) {
+	fmt.Printf("NKS CLI Version - v%s (alpha)\n", v)
 }
 
 func init() {
