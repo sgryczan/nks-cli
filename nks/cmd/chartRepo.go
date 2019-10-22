a9a043e14dc623573798e35d3860d693fc13865d
Sebastian Gryczan <sgryczan@netapp.com>
2019-10-22 15:08:22 -0600

    tweak formatting

--- nks/cmd/chartRepo.go
+++ nks/cmd/chartRepo.go
@@ -256,10 +256,10 @@ func printRepositories(rs []models.Repository) {
 }
 
 func printCharts(chartIndexes []models.ChartIndex) {
-	w := tabwriter.NewWriter(os.Stdout, 0, 10, 5, ' ', 0)
+	w := tabwriter.NewWriter(os.Stdout, 0, 5, 5, ' ', 0)
 	fmt.Fprintf(w, "NAME\tDESCRIPTION\tVERSION\tPATH\t\n")
 	for _, c := range chartIndexes {
-		fmt.Fprintf(w, "%v\t%v\t%v\t%v\t\n", c.Name, c.Chart.Description, c.Chart.AppVersion, c.Path)
+		fmt.Fprintf(w, "%v\t%.50s\t%v\t%v\t\n", c.Name, c.Chart.Description, c.Chart.AppVersion, c.Path)
 	}
 	w.Flush()
 }
